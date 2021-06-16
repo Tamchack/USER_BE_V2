1b546640acf1b36d39c8611b04eb438ce2263b66
임서영 <sylim0318@naver.com>
2021-06-16 23:34:59 +0900

    [ADD] 서점 디티오

---
+++ src/main/java/com/tamchack/admin/payload/response/StoreResponse.java
@@ -0,0 +1,18 @@
+package com.tamchack.admin.payload.response;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreResponse {
+
+    private int id;
+
+    private String name;
+
+    private String owner;
+
+}
