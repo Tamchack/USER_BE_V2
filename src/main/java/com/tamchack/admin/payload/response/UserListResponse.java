b021c416e4b4d6db7f52fcfcdabe135ba83e4f11
임서영 <sylim0318@naver.com>
2021-06-17 16:54:43 +0900

    [FIX] 사용자 리스트 디티오

--- src/main/java/com/tamchack/admin/payload/response/UserListResponse.java
+++ src/main/java/com/tamchack/admin/payload/response/UserListResponse.java
@@ -1,12 +1,14 @@
 package com.tamchack.admin.payload.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class UserListResponse {
 
     private int totalPages;
