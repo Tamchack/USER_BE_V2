25ea11908716cfc0c4a4734f1f77d330fefb66ce
임서영 <sylim0318@naver.com>
2021-06-18 15:56:12 +0900

    [ADD] 서점 주인 레포지토리

--- src/main/java/com/tamchack/tamchack/repository/member/StoreuserRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/member/StoreuserRepository.java
@@ -1,6 +1,8 @@
 package com.tamchack.tamchack.repository.member;
 
 import com.tamchack.tamchack.domain.member.Storeuser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +10,7 @@ import java.util.Optional;
 public interface StoreuserRepository extends JpaRepository<Storeuser, String> {
 
     Optional<Storeuser> findByIdAndPassword(String id, String password);
+
+    Page<Storeuser> findAllBy(Pageable pageable);
 
 }
