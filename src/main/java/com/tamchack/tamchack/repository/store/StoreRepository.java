882a781f23a3c65345737e5080f8e621791651ad
yujina <jina6962@gmail.com>
2021-06-21 23:37:02 +0900

    [ADD]

--- src/main/java/com/tamchack/tamchack/repository/store/StoreRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/store/StoreRepository.java
@@ -4,7 +4,10 @@ import com.tamchack.tamchack.domain.store.Store;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Integer> {
@@ -14,5 +17,10 @@ public interface StoreRepository extends JpaRepository<Store, Integer> {
     Store findByNumber(String number);
 
     Store findByOpeningHours(String openingHours);
+
+    @Query(value = "SELECT * (6371*acos(cos(radians(?1))*cos(radians(lat))*cos(radians(lng)" +
+                    "-radians(?2))+sin(radians(?1))*sin(radians(lat)))) " +
+                    "AS distance FROM tbl_store HAVING distance <= 1", nativeQuery = true)
+    List<Store> findByLocation(double lat, double lng);
 
 }
