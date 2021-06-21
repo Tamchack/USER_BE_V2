aca451e61b535008628a8fcb018c9f199baaea2c
yujina <jina6962@gmail.com>
2021-06-21 23:36:45 +0900

    [ADD]

--- src/main/java/com/tamchack/tamchack/repository/book/BookRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/book/BookRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository <Book, Integer> {
 
     Page<Book> findAllByNameContains(String bookName, Pageable page);
 
     Page<Book> findAllByStoreIdAndNameContains(Integer storeId, String bookName, Pageable page);
+
+    Optional<Book> findAllByName(String name);
 
 }
