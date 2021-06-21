31f1736860ed759a19da5dd9d9f3ae8fe3e96a58
yujina <jina6962@gmail.com>
2021-06-21 23:38:01 +0900

    [ADD]

--- src/main/java/com/tamchack/tamchack/service/store/StoreService.java
+++ src/main/java/com/tamchack/tamchack/service/store/StoreService.java
@@ -1,11 +1,14 @@
 package com.tamchack.tamchack.service.store;
 
+import com.tamchack.tamchack.domain.store.Store;
 import com.tamchack.tamchack.dto.request.store.BookmarkRequest;
 import com.tamchack.tamchack.dto.request.store.DeclarationStoreRequest;
 import com.tamchack.tamchack.dto.request.member.ReviseInformationRequest;
 import com.tamchack.tamchack.dto.response.address.ApplicationListResponse;
 import com.tamchack.tamchack.dto.response.store.StoreResponse;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface StoreService {
 
@@ -14,6 +17,8 @@ public interface StoreService {
     void DeclarationStore(DeclarationStoreRequest declarationStoreRequest);
 
     ApplicationListResponse searchStore(String query, Pageable page);
+
+    List<StoreResponse> searchStorePlace(double lat, double lng);
 
     StoreResponse getStore(Integer storeId);
 
