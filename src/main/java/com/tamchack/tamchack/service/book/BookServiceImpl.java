f31279fe2f6d2b64ce85257823a8dd691c2988c6
yujina <jina6962@gmail.com>
2021-06-21 23:37:21 +0900

    [FIX]

--- src/main/java/com/tamchack/tamchack/service/book/BookServiceImpl.java
+++ src/main/java/com/tamchack/tamchack/service/book/BookServiceImpl.java
@@ -4,33 +4,34 @@ import com.tamchack.tamchack.domain.book.Book;
 import com.tamchack.tamchack.domain.book.Stock;
 import com.tamchack.tamchack.domain.member.Storeuser;
 import com.tamchack.tamchack.domain.member.User;
-import com.tamchack.tamchack.domain.store.Store;
 import com.tamchack.tamchack.dto.request.book.BookRequest;
 import com.tamchack.tamchack.dto.request.book.DeclarationBookRequest;
 import com.tamchack.tamchack.dto.request.book.StockRequest;
 import com.tamchack.tamchack.dto.response.address.ApplicationListResponse;
 import com.tamchack.tamchack.dto.response.book.BookResponse;
-import com.tamchack.tamchack.dto.response.store.StoreResponse;
+import com.tamchack.tamchack.exception.BookAlreadyExistsException;
 import com.tamchack.tamchack.exception.BookNotFoundException;
-import com.tamchack.tamchack.exception.StoreNotFoundException;
-import com.tamchack.tamchack.exception.UserAlreadyEsixtsException;
 import com.tamchack.tamchack.exception.UserNotFoundException;
 import com.tamchack.tamchack.repository.book.BookRepository;
 import com.tamchack.tamchack.repository.book.DeclarationBookRepository;
+import com.tamchack.tamchack.repository.book.ImageRepository;
 import com.tamchack.tamchack.repository.book.StockRepository;
 import com.tamchack.tamchack.repository.member.StoreuserRepository;
 import com.tamchack.tamchack.repository.member.UserRepository;
 import com.tamchack.tamchack.security.token.JWTProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -42,11 +43,15 @@ public class BookServiceImpl implements BookService {
     private final UserRepository userRepository;
     private final StoreuserRepository storeuserRepository;
     private final DeclarationBookRepository declarationBookRepository;
+    private final ImageRepository imageRepository;
     private final JWTProvider jwtProvider;
+
+    @Value("${book.image.path}")
+    private String imagePath;
 
     @SneakyThrows
     @Override //책 등록
-    public void inputBook(BookRequest bookRequest, String token) {
+    public void inputBook(BookRequest bookRequest, String name, String token) {
 
         User user = userRepository.findById(jwtProvider.parseToken(token))
                 .orElseThrow(UserNotFoundException::new);
@@ -54,12 +59,14 @@ public class BookServiceImpl implements BookService {
         Storeuser storeuser = storeuserRepository.findById(jwtProvider.parseToken(token))
                 .orElseThrow(UserNotFoundException::new);
 
+        Book book = bookRepository.findAllByName(name)
+                .orElseThrow(BookAlreadyExistsException::new);
 
         String fileName = UUID.randomUUID().toString();
 
         File file = new File(imagePath, fileName);
 
-        Book book = bookRepository.save(
+        bookRepository.save(
                 Book.builder()
                         .name(bookRequest.getName())
                         .author(bookRequest.getAuthor())
@@ -84,7 +91,7 @@ public class BookServiceImpl implements BookService {
                 .author(book.getAuthor())
                 .publisher(book.getPublisher())
                 .contents(book.getContents())
-                .imageName()
+                .imageName(book.getImageName())
                 .build();
 
     }
