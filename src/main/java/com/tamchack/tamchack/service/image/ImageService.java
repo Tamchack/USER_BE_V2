1685be84e1236a0e11043a369813c55ac6394684
yujina <jina6962@gmail.com>
2021-06-21 13:42:31 +0900

    [ADD]

---
+++ src/main/java/com/tamchack/tamchack/service/image/ImageService.java
@@ -0,0 +1,7 @@
+package com.tamchack.tamchack.service.image;
+
+public interface ImageService {
+
+    byte[] getImage(String imageName);
+
+}
---
+++ src/main/java/com/tamchack/tamchack/service/image/ImageServiceImpl.java
@@ -0,0 +1,32 @@
+package com.tamchack.tamchack.service.image;
+
+import com.tamchack.tamchack.exception.ImageNotFoundException;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@Service
+public class ImageServiceImpl implements ImageService{
+
+
+    @Value("${book.image.path}")
+    private String imagePath;
+
+    @SneakyThrows
+    @Override
+    public byte[] getImage(String imageName) {
+        File file = new File(imagePath, imageName);
+        if (!file.exists()){
+            throw new ImageNotFoundException();
+        }
+        InputStream inputStream = new FileInputStream(file);
+
+        return IOUtils.toByteArray(inputStream);
+    }
+    
+}
