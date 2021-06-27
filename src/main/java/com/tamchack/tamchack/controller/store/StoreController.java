dfdca48729cc6c0efc008ec02da28996bc619468
yujina <jina6962@gmail.com>
2021-06-27 22:48:17 +0900

    [ADD]

--- src/main/java/com/tamchack/tamchack/controller/store/StoreController.java
+++ src/main/java/com/tamchack/tamchack/controller/store/StoreController.java
@@ -1,5 +1,8 @@
 package com.tamchack.tamchack.controller.store;
 
+import com.tamchack.tamchack.dto.request.store.BookmarkRequest;
+import com.tamchack.tamchack.dto.request.store.DeclarationStoreRequest;
+import com.tamchack.tamchack.dto.response.address.ApplicationListResponse;
 import com.tamchack.tamchack.dto.response.store.StoreResponse;
 import com.tamchack.tamchack.service.book.BookService;
 import com.tamchack.tamchack.service.store.StoreService;
@@ -9,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.List;
 
 @RestController
 @RequestMapping("/store")
@@ -18,14 +22,48 @@ public class StoreController {
     private final StoreService storeService;
     private final BookService bookService;
 
-    @GetMapping("/search")
-    public void searchStore(@RequestParam String name,
-                            @PageableDefault Pageable pageable){
-        storeService.searchStore(name, pageable);
+    @PutMapping("/{storeId")
+    public void bookmarkStore(@RequestBody BookmarkRequest bookmarkRequest) {
+
+        storeService.bookmarkStore(bookmarkRequest);
+
+    }
+
+    @DeleteMapping("/{storeId}")
+    public void DeclarationStore(@RequestBody DeclarationStoreRequest declarationStoreRequest) {
+
+        storeService.DeclarationStore(declarationStoreRequest);
+
     }
 
     @GetMapping("/{storeId}")
     public StoreResponse getStore(@PathVariable Integer storeId){
+
         return storeService.getStore(storeId);
+
     }
+
+    @GetMapping("/search")
+    public ApplicationListResponse searchStore(@RequestParam String query, @PageableDefault Pageable page) {
+
+        return storeService.searchStore(query, page);
+
+    }
+
+    public List<StoreResponse> searchStorePlace(@RequestParam double lat,
+                                                @RequestParam double lng) {
+
+        return storeService.searchStorePlace(lat, lng);
+
+    }
+
+    @GetMapping("/{storeId}")
+    public ApplicationListResponse searchBookInStore(@RequestBody Integer storeId,
+                                                     @RequestBody String query,
+                                                     @PageableDefault Pageable page) {
+
+        return bookService.searchBookInStore(storeId, query, page);
+
+    }
+
 }
