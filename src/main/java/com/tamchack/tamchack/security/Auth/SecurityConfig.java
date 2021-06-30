4c2e83d522ced678238d61eee6901798a1bdfda7
yujina <jina6962@gmail.com>
2021-06-30 20:06:10 +0900

    [ADD] - Password Encoder

--- src/main/java/com/tamchack/tamchack/security/Auth/SecurityConfig.java
+++ src/main/java/com/tamchack/tamchack/security/Auth/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.tamchack.tamchack.security.Auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -27,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .formLogin().disable()
                 .authorizeRequests().anyRequest()
                 .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
