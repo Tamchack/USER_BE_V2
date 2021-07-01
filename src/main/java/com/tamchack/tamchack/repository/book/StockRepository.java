05c5115fc957c3e722e43757cc980feb61256099
yujina <jina6962@gmail.com>
2021-07-01 23:31:47 +0900

    [UPDATE] - 변수 이름, 타입

--- src/main/java/com/tamchack/tamchack/repository/book/StockRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/book/StockRepository.java
@@ -3,14 +3,15 @@ package com.tamchack.tamchack.repository.book;
 import com.tamchack.tamchack.domain.book.Book;
 import com.tamchack.tamchack.domain.book.Stock;
 import com.tamchack.tamchack.domain.book.StockKey;
+import com.tamchack.tamchack.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, StockKey> {
 
-    boolean existsByStoreIdAndBook(Integer storeId, Book book);
+    boolean existsByStoreAndBook(Store store, Book book);
 
-    void deleteByStoreIdAndBook(Integer storeId, Book book);
+    void deleteByStoreAndBook(Store store, Book book);
 
 }
