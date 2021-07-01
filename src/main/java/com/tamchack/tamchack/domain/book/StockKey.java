4064aa909f17a4faec88dcf16f60e10fe8604fa1
yujina <jina6962@gmail.com>
2021-07-01 23:26:50 +0900

    [UPDATE] - type

--- src/main/java/com/tamchack/tamchack/domain/book/StockKey.java
+++ src/main/java/com/tamchack/tamchack/domain/book/StockKey.java
@@ -1,5 +1,6 @@
 package com.tamchack.tamchack.domain.book;
 
+import com.tamchack.tamchack.domain.store.Store;
 import lombok.Builder;
 
 import java.io.Serializable;
@@ -7,7 +8,7 @@ import java.io.Serializable;
 @Builder
 public class StockKey implements Serializable {
 
-    private int storeId;
+    private Store store;
 
     private Book book;
 
