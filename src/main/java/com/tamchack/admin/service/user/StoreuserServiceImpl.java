98dacdde6d6d876fa1c3058696daba1ea56d4a74
임서영 <sylim0318@naver.com>
2021-07-12 17:20:18 +0900

    [ADD] 서점 주인 서비스

---
+++ src/main/java/com/tamchack/admin/service/user/StoreuserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.tamchack.admin.service.user;
+
+import com.tamchack.admin.payload.response.StoreUserListResponse;
+import com.tamchack.admin.payload.response.StoreUserResponse;
+import com.tamchack.tamchack.domain.member.Storeuser;
+import com.tamchack.tamchack.repository.member.StoreuserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class StoreuserServiceImpl implements StoreuserService {
+
+    private final StoreuserRepository storeuserRepository;
+
+    @Override
+    public StoreUserListResponse getListUser(Pageable pageable) {
+        Page<Storeuser> stores = storeuserRepository.findAllBy(pageable);
+
+        int totalPages = stores.getTotalPages();
+
+        List<StoreUserResponse> mapStoreusers = stores.map(this::buildStoreuser)
+                .stream().collect(Collectors.toList());
+
+        return new StoreUserListResponse(totalPages, mapStoreusers);
+    }
+
+    private StoreUserResponse buildStoreuser(Storeuser storeuser) {
+        return StoreUserResponse.builder()
+                .id(storeuser.getId())
+                .name(storeuser.getStore().getName())
+                .build();
+    }
+
+}
