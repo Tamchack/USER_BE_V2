3d91700a9fe372305e5f1c84ad6e782e559df725
임서영 <sylim0318@naver.com>
2021-07-30 20:30:48 +0900

    [ADD] 유저 삭제 추가

--- src/main/java/com/tamchack/admin/controller/user/UserController.java
+++ src/main/java/com/tamchack/admin/controller/user/UserController.java
@@ -4,19 +4,23 @@ import com.tamchack.admin.payload.response.UserListResponse;
 import com.tamchack.admin.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/admin/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping
-    public UserListResponse getUsers(Pageable pageable) {
-
+    @GetMapping("/list")
+    public UserListResponse getUsers(@RequestParam Pageable pageable) {
         return userService.getListUser(pageable);
-
     }
+
+    @DeleteMapping("{/userId}")
+    public void deleteUser(@PathVariable String userId) {
+        userService.deleteUser(userId);
+    }
+
 }
