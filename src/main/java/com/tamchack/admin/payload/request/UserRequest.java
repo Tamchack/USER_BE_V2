415f600118fb3d3508c2d68630403c03b469e235
임서영 <sylim0318@naver.com>
2021-07-31 23:59:31 +0900

    [ADD] 유저 디티오

---
+++ src/main/java/com/tamchack/admin/payload/request/UserRequest.java
@@ -0,0 +1,14 @@
+package com.tamchack.admin.payload.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+
+    private String UserId;
+
+}
