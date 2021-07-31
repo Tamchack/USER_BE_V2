52569d65876f7de7535765ef53f66272e6c31bc5
임서영 <sylim0318@naver.com>
2021-08-01 00:52:46 +0900

    [ADD] 서점주인 삭제 컨트롤러 추가

--- src/main/java/com/tamchack/admin/controller/user/StoreUserController.java
+++ src/main/java/com/tamchack/admin/controller/user/StoreUserController.java
@@ -4,19 +4,24 @@ import com.tamchack.admin.payload.response.StoreListResponse;
 import com.tamchack.admin.service.store.StoreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/admin/storeuser")
 @RequiredArgsConstructor
 public class StoreUserController {
 
     private final StoreService storeService;
 
-    @GetMapping
-    public StoreListResponse getStoreUsers(Pageable pageable) {
+    @GetMapping("/list")
+    public StoreListResponse getStoreUsers(@RequestParam Pageable pageable) {
 
         return storeService.getListStore(pageable);
 
     }
+    @DeleteMapping("{/storeuserId}")
+    public void deleteStoreuser(@PathVariable int storeuserId) {
+        storeService.deleteStore(storeuserId);
+    }
+
 }
