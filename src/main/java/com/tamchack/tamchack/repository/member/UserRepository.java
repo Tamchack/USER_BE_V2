4f0996cf07f87c4f6dcfd4ea409ca3ece9bbabdc
임서영 <sylim0318@naver.com>
2021-08-10 18:50:42 +0900

    [FIX] 어노테이션 추가

--- src/main/java/com/tamchack/tamchack/repository/member/UserRepository.java
+++ src/main/java/com/tamchack/tamchack/repository/member/UserRepository.java
@@ -4,9 +4,11 @@ import com.tamchack.tamchack.domain.member.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository <User, String> {
 
     Optional<User> findByIdAndPassword(String id, String password);
