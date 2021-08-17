de6756db45711bff0234bb1e58a5a25fbb82a24b
임서영 <sylim0318@naver.com>
2021-08-17 13:42:51 +0900

    [FIX] url 수정

--- src/main/java/com/tamchack/admin/controller/bestseller/BestSellerController.java
+++ src/main/java/com/tamchack/admin/controller/bestseller/BestSellerController.java
@@ -4,19 +4,23 @@ import com.tamchack.admin.payload.response.BestSellerListResponse;
 import com.tamchack.admin.service.bestseller.BestSellerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/admin/bestseller")
 @RequiredArgsConstructor
 public class BestSellerController {
 
     private final BestSellerService bestSellerService;
 
-    @GetMapping("/best/list")
-    public BestSellerListResponse getBooks(Pageable pageable) {
-
+    @GetMapping("/list")
+    public BestSellerListResponse getBooks(@RequestParam Pageable pageable) {
         return bestSellerService.getListBestSeller(pageable);
-
     }
+
+    @GetMapping("/management")
+    public BestSellerListResponse getBestSeller() {
+        return bestSellerService.choiceBestSeller();
+    }
+
 }
