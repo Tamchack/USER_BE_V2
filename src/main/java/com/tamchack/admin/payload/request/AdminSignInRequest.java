8b375caf74f00c1de912f373363a06df960e960e
임서영 <sylim0318@naver.com>
2021-08-17 13:43:49 +0900

    [FIX] 클래스명 변경

--- src/main/java/com/tamchack/admin/payload/request/AdminSignInRequest.java
+++ src/main/java/com/tamchack/admin/payload/request/AdminSignInRequest.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class SignInRequest {
+public class AdminSignInRequest {
 
     private String id;
     private String password;
